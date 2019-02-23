f8afdcaeb9b27b4117a57529f0d7bb54190408f1
Akash Juneja <akashjuneja029@gmail.com>
2019-02-23 23:30:32 +0530

    First Question is done

---
+++ Question 1/index.js
@@ -0,0 +1,12 @@
+
+ function simple() {
+
+    let a;
+    if (typeof (a))
+        console.log(typeof (a)); //undefined error
+    const b = 10;
+    if (typeof (b))
+        console.log(typeof (b)); //it will give you a number
+
+}
+test();
